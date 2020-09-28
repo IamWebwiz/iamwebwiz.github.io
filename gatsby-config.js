822635baf5c1038fa15e15fa6486d5ff1f21f8b3
Ezekiel <iamwebwiz@gmail.com>
2020-09-28 17:09:40 +0100

    add tido config & environment variables

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-plugin-tidio-chat",
+      options: {
+        tidioKey: process.env.TIDIO_PUBLIC_KEY,
+        enableDuringDevelop: true,
+      },
+    },
+  ],
+};
